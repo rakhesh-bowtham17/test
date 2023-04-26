b65fe2f36a4ba930d32267f322a665a0cbb8d335
Hariboobaalan <hariboobaalan.palanisamy@cdw.com>
2023-04-26 22:03:09 +0530

    delete all

--- src/constants/data-constant.js
+++
@@ -1,7 +0,0 @@
-export const HEADER = "Welcome to wallet";
-export const CASH_HEADING = "CDW CASH REPORT";
-export const DASHBOARD_HEADING = "DASHBOARD";
-export const DEFAULT_AMOUNT = 400;
-
-
-
--- src/utils/logoHighLighter.js
+++
@@ -1,7 +0,0 @@
-function highLighter(name) {
-  const normalText = name.slice(0, -2);
-  const highLightedText = name.slice(-2);
-
-  return [normalText, highLightedText];
-}
-export default highLighter;
